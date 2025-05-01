996c331082c40f54554d0410283e64209a4b5bc0
cactoes <maartenhvanrooij@gmail.com>
2025-05-01 20:40:36 +0200

    done with most of login page

--- frontend/scripts/login.ts
+++ frontend/scripts/login.ts
@@ -1,11 +1,37 @@
-function get_input(elementid: string): string | undefined {
-    return (document.getElementById(elementid) as HTMLInputElement | null)?.value;
+async function submit_callback() {
+    const username_input = get_element<HTMLInputElement>("input#username");
+    const password_input = get_element<HTMLInputElement>("input#password");
+
+    if (!username_input.value)
+        get_element<HTMLInputElement>("input#username").classList.add("error");
+    
+    if (!password_input.value)
+        get_element<HTMLInputElement>("input#password").classList.add("error");
+
+    if (!username_input.value || !password_input.value)
+        return;
+
+    username_input.classList.remove("error");
+    password_input.classList.remove("error");
+
+    const result = await make_api_call("POST", "/account/login", { username: username_input.value, password: await sha256(password_input.value) }) as { error: boolean, message: string, payload: { token?: string } };
+    
+    if (result.error) {
+        get_element<HTMLInputElement>("input#username").classList.add("error");
+        get_element<HTMLInputElement>("input#password").classList.add("error");
+        return;
+    }
+
+    set_jwt(result.payload.token!);
+
+    window.location.href = "/home";
 }
 
-function submit_callback() {
-    console.log(get_input("input#username"), get_input("input#password"));
+function remove_error(event: Event) {
+    (event.target as HTMLInputElement).classList.remove("error")
 }
 
-link_component("button#submit", {
-    click: submit_callback
-});
+link_component("input#username", { input: remove_error });
+link_component("input#password", { input: remove_error });
+
+link_component("button#submit", { click: submit_callback });
