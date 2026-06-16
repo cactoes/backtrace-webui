73158853d785b19c1bfd7830c55ccb124f069255
cactoes <maartenhvanrooij@gmail.com>
2026-06-17 00:32:17 +0200

    more ui changes

--- frontend/scripts/index.ts
+++ frontend/scripts/index.ts
@@ -1,93 +1,36 @@
-// eslint-disable-next-line
-// @ts-ignore
-VanillaTilt.init(document.querySelector(".background"), {
-    reverse: true,
-    speed: 600,
-    perspective: 2000
-});
-
-util.make_interval(() => {
-    const fmt_time = (value: number): string => (value < 10 ? `0${value}` : `${value}`);
-
-    const now = new Date();
-
-    const [ hours, minutes, seconds ] = [
-        fmt_time(now.getHours()),
-        fmt_time(now.getMinutes()),
-        fmt_time(now.getSeconds())
-    ];
-
-    document.getElementById("clock")!.innerText =
-        `${hours} : ${minutes} : ${seconds}`;
-}, 1000);
-
-function ease_inout_sine(x: number): number {
-    return -(Math.cos(Math.PI * x * 2) - 1) / 2;
-}
-
-const canvas = document.getElementById("stars") as HTMLCanvasElement;
-const context = canvas.getContext("2d")!;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.onresize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-
-class star {
-    public current_fade_tick: number = 0;
-
-    constructor(
-        private x: number,
-        private y: number,
-        public max_fade_tick: number,
-        private size: number,
-    ) {}
-
-    public static create(): star {
-        return new star(
-            Math.random() * window.innerWidth,
-            Math.random() * window.innerHeight,
-            Math.round(Math.random() * 3 + 4),
-            Math.round(Math.random() * 2 + 1)
-        );
-    }
-
-    public update(dt: number): boolean {
-        this.current_fade_tick += 1 * dt;
-        return this.current_fade_tick < this.max_fade_tick;
-    }
-
-    public render(): void {
-        const scale = (this.max_fade_tick - this.current_fade_tick) / (this.max_fade_tick);
-        context.fillStyle = `rgba(255, 255, 255, ${ease_inout_sine(scale)})`;
-        context.beginPath();
-        context.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        context.fill();
-    }
-}
-
-const star_array = Array.from<star>({ length: 200 }).map(st => {
-    st = star.create();
-    st.current_fade_tick = Math.random() * st.max_fade_tick;
-    return st;
-});
-
-function render(delta_time: number) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    star_array.forEach((st, i) => {
-        if (!st.update(delta_time))
-            star_array[i] = star.create();
-
-        st.render();
+async function main(): Promise<void> {
+    util.check_logged_in().then(is_logged_in => {
+        const login_button = document.getElementById("login");
+        const home_button = document.getElementById("home");
+        if (is_logged_in) {
+            if (login_button)
+                login_button.style.display = "none";
+            
+        } else {
+            login_button?.addEventListener("click", () => window.location.href = "/login");
+            if (home_button)
+                home_button.style.display = "none";
+        }
     });
 
-    context.fillStyle = "rgb(0, 0, 0)";
-    context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, 200, 0, 2 * Math.PI);
-    context.fill();
+    util.make_api_call<{ ui: string, api: any, proxy: any }>("GET", "/version").then(result => {
+        element.get<HTMLDivElement>("ver").innerText = `v${result!.payload!.ui}`;
+    });
+
+    util.make_interval(() => {
+        const fmt_time = (value: number): string => (value < 10 ? `0${value}` : `${value}`);
+
+        const now = new Date();
+
+        const [ hours, minutes, seconds ] = [
+            fmt_time(now.getHours()),
+            fmt_time(now.getMinutes()),
+            fmt_time(now.getSeconds())
+        ];
+
+        document.getElementById("time")!.innerText =
+            `${hours} : ${minutes} : ${seconds}`;
+    }, 1000);
 }
 
-util.start_canvas_render_loop(render);
+window.addEventListener("DOMContentLoaded", main);
