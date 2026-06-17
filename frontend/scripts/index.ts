05ee4314f825c1ba18e8a6c33da4cc92e6a88fe8
cactoes <maartenhvanrooij@gmail.com>
2026-06-17 20:35:36 +0200

    more ui updates

--- frontend/scripts/index.ts
+++ frontend/scripts/index.ts
@@ -1,3 +1,49 @@
+function format_time(date: Date) {
+    const dt_ms = Date.now() - date.getTime();
+    const dt_days = Math.floor(dt_ms / (1000 * 60 * 60 * 24));
+
+    return dt_days == 0
+        ? "today"
+        : dt_days == 1
+        ? "1 day ago"
+        : `${dt_days} days ago`;
+}
+
+async function get_repo(project: string) {
+    const res = await fetch(`https://api.github.com/repos/cactoes/${project}`);
+    if (!res.ok)
+        return undefined;
+
+    return res.json();
+}
+
+function setup_card(card: Element) {
+    const project = card.getAttribute("project");
+    if (!project)
+        return;
+
+    card.addEventListener("click", () => {
+        window.open(`https://github.com/cactoes/${project}`, "_blank");
+    });
+
+    const target_element = card.querySelector(".card-footer>.updated");
+
+    get_repo(project).then(data => {
+        if (!data) {
+            if (target_element)
+                target_element.textContent = "? days ago";
+
+            return;
+        }
+
+        const date = new Date(data.updated_at);
+        const label = format_time(date);
+
+        if (target_element)
+            target_element.textContent = label;
+    });
+}
+
 // @ts-ignore
 async function main(): Promise<void> {
     util.check_logged_in().then(is_logged_in => {
@@ -32,6 +78,10 @@ async function main(): Promise<void> {
         document.getElementById("time")!.innerText =
             `${hours} : ${minutes} : ${seconds}`;
     }, 1000);
+
+    document
+        .querySelectorAll("section>span")
+        .forEach(setup_card);
 }
 
 window.addEventListener("DOMContentLoaded", main);
